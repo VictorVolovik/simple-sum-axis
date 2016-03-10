0f5f4e85b7b828a21ce6d6dd48c2123daf8c355b
Victor Volovik <andhope2die@gmail.com>
2016-03-10 21:52:20 +0300

    terms generating

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,42 @@
-var a = 42;
+// prerequisite arrays
+var firstTermArray = [6, 7, 8, 9];
+var secondTermArray = [];
+var sumArray = [11, 12, 13, 14];
+
+for (
+  var i = sumArray[0] - firstTermArray[firstTermArray.length - 1]; i < (sumArray[sumArray.length - 1] - firstTermArray[0]); i++
+) {
+  secondTermArray.push(i);
+}
+
+var firstTerm;
+var secondTerm;
+var sumOfTerms = firstTerm + secondTerm;
+
+var firstTermPre = $('#first-term-pre');
+var secondTermPre = $('#second-term-pre');
+var sum = $('#sum');
+
+var randomTerm = function (array) {
+  return array[Math.floor(Math.random() * array.length)];
+};
+
+//
+var generateTerms = function () {
+  do {
+    firstTerm = randomTerm(firstTermArray);
+    secondTerm = randomTerm(secondTermArray);
+    console.log(firstTerm);
+    console.log(secondTerm);
+  } while ((firstTerm + secondTerm) < sumArray[0] || (firstTerm + secondTerm) > sumArray[sumArray.length - 1]);
+};
+
+var renderTerms = function (a, b) {
+  firstTermPre.text(a);
+  secondTermPre.text(b);
+};
+
 $(document).ready(function () {
-  $('h1').text(a);
+  generateTerms();
+  renderTerms(firstTerm, secondTerm);
 });
